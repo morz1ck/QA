b5dfe03fc752f4a6129bdc76826232009e991177
morz1ck <120595021+morz1ck@users.noreply.github.com>
2025-03-20 15:21:46 +0300

    Add files via upload
    генерация данных для таблицы sql

---
+++ generation sql.py
@@ -0,0 +1,55 @@
+import psycopg2
+import random
+from faker import Faker
+
+# Подключение к базе данных
+conn = psycopg2.connect(
+    dbname="university",
+    user="postgres",
+    password="a14092006",
+    host="localhost",
+    port="1409"
+)
+cur = conn.cursor()
+faker = Faker()
+
+for _ in range(10):
+    name = faker.name()
+    start_year = random.randint(2021, 2024)
+    cur.execute("INSERT INTO Students (name, start_year) VALUES (%s, %s) RETURNING s_id", (name, start_year))
+
+
+cur.execute("SELECT s_id FROM Students")
+students = [row[0] for row in cur.fetchall()]
+
+
+if not students:
+    print("таблица students пуста")
+    exit()
+
+cur.execute("SELECT c_no FROM Courses")
+courses = [row[0] for row in cur.fetchall()]
+
+
+if not courses:
+    print("таблица courses пуста")
+    exit()
+
+for _ in range(20):
+    s_id = random.choice(students)
+    c_no = random.choice(courses)
+    score = random.randint(2, 5)
+
+    cur.execute("""
+        INSERT INTO Exams (s_id, c_no, score) 
+        VALUES (%s, %s, %s) 
+        ON CONFLICT (s_id, c_no) DO UPDATE 
+        SET score = EXCLUDED.score
+    """, (s_id, c_no, score))
+
+
+conn.commit()
+cur.close()
+conn.close()
+
+print("2 котята и пирожки")
